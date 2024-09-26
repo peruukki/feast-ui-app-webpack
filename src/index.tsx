7269a4bfb68fff98e82ed9269712b32acb20e947
Harri Lehtola <peruukki@hotmail.com>
2024-09-26 21:54:42 +0300

    Working with React 17.0.2

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 
 import './index.css';
 
 import App from './app';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App />);
+ReactDOM.render(<App />, document.getElementById('root'));

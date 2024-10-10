01e02140a9e364e618a65d5c903b0ecc5e0bf059
Harri Lehtola <peruukki@hotmail.com>
2024-10-10 21:36:41 +0300

    Upgrade React from 17.0.2 to 18.3.1

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 import './index.css';
 
 import App from './app';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(<App />);

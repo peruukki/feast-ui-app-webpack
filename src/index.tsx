da8a0021d9b35f170f75e8b23c91ae2b88d893ca
Harri Lehtola <peruukki@hotmail.com>
2024-10-11 21:14:10 +0300

    Feast UI with DemoCustomTab

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,26 @@
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
-
-import App from './app';
+import FeastUI from '@feast-dev/feast-ui';
+import '@feast-dev/feast-ui/dist/feast-ui.css';
+import DemoCustomTab from './DemoCustomTab';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
-root.render(<App />);
+root.render(
+  <StrictMode>
+    <FeastUI
+      feastUIConfigs={{
+        tabsRegistry: {
+          EntityCustomTabs: [
+            {
+              label: 'Custom Tab Demo', // Navigation Label for the tab
+              path: 'demo-tab', // Subpath for the tab
+              Component: DemoCustomTab, // a React Component
+            },
+          ],
+        },
+      }}
+    />
+  </StrictMode>
+);
